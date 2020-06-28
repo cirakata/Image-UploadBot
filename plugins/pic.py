6bac27ceb82ab11e2e3c701ad0847c67d15937c0
cirakata <63696553+cirakata@users.noreply.github.com>
2020-06-28 14:37:46 +0530

    Update pic.py

--- plugins/pic.py
+++ plugins/pic.py
@@ -9,7 +9,7 @@ from telegraph import upload_file
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by @nitishjeet7bot",
+        text=f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by [Πī†īsHjēē†7 ™](https://t.me/nitishjeet7)",
         reply_to_message_id=message.message_id
     )
     
